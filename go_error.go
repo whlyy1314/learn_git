97d521e5981a6124e4e992a7bf92b55e4be14518
whlyy1314 <82764921+whlyy1314@users.noreply.github.com>
2021-04-25 22:16:22 +0800

    Create go_error.go

---
+++ go_error.go
@@ -0,0 +1,26 @@
+import ( 
+   "database/sql" 
+   "fmt" 
+ 
+   "github.com/pkg/errors" 
+) 
+ 
+func GetSql() error { 
+   return errors.Wrap(sql.ErrNoRows, "GetSql failed") 
+} 
+ 
+func Call() error { 
+   return errors.WithMessage(GetSql(), "bar failed") 
+} 
+ 
+func main() { 
+   err := Call() 
+   if errors.Cause(err) == sql.ErrNoRows { 
+      fmt.Printf("data not found, %v\n", err) 
+      fmt.Printf("%+v\n", err) 
+      return 
+   } 
+   if err != nil { 
+      // unknown error 
+   } 
+} 
